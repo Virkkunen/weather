0eaa88a944540f5fba945a672bb7b90e81a9736f
Virkkunen <vrkknn@vrkknn.net>
2023-03-28 19:49:43 -0300

    fix: proper usage of gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/weather/',
 })
