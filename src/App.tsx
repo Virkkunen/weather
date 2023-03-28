4908e470a8e67f09d1428d58d022a27038fe172e
Virkkunen <vrkknn@vrkknn.net>
2023-03-28 14:01:05 -0300

    feat: start repo with vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-base text-pink">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
