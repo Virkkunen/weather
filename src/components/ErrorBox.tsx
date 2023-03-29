0a01f3fccaeceae2f58e85facfaf77f6bdb57a50
Virkkunen <vrkknn@vrkknn.net>
2023-03-29 11:20:23 -0300

    feat: close button for error box

--- src/components/ErrorBox.tsx
+++ src/components/ErrorBox.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import WeatherContext from '../context/WeatherContext';
 
 const ErrorBox = () => {
-  const { error } = useContext(WeatherContext);
+  const { error, setError } = useContext(WeatherContext);
 
   interface ErrorMap {
     [key: string]: string;
@@ -13,11 +13,20 @@ const ErrorBox = () => {
     404: `City not found.`,
   };
 
-  return (
-    <div className='m-4 p-8 rounded-lg bg-surface0 flex flex-col place-content-center drop-shadow hover:drop-shadow-lg'>
-      <span>{errorMap[error.code]}</span>
-    </div>
-  )
-}
+  const handleClick = () => setError(null);
 
-export default ErrorBox
+  return (
+    <div className='m-4 p-8 rounded-lg bg-surface0 flex flex-col place-content-center drop-shadow hover:drop-shadow-lg relative ease-in-out duration-200'>
+      <span>{errorMap[error.code]}</span>
+      <button
+        type='button'
+        className='font-bold text-maroon mt-4 hover:text-red active:text-opacity-60 ease-in-out duration-200'
+        onClick={handleClick}
+      >
+        Close
+      </button>
+    </div>
+  );
+};
+
+export default ErrorBox;
