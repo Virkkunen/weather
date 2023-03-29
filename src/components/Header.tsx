eb77b9510c99294fe45dc3aa93729a163c7ce7ae
Virkkunen <vrkknn@vrkknn.net>
2023-03-29 12:17:37 -0300

    feat: header layout changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,34 @@
 import { useContext } from 'react';
 import WeatherContext from '../context/WeatherContext';
 import { SearchBar } from './SearchBar';
+import { FaSearch, FaStar, FaRegStar } from 'react-icons/fa';
 
 export default function Header() {
-  const { isLoading, weatherData } = useContext(WeatherContext);
+  const { weatherData } = useContext(WeatherContext);
   return (
-    <div className='container mx-auto text-center p-8 mb-4 rounded-b-lg bg-base drop-shadow'>
-      {weatherData && <span className='text-xl'>{weatherData.name}</span>}
-      <SearchBar />
-    </div>
+    <>
+      <div className='container grid grid-flow-col grid-cols-6 grid-rows-2 mx-auto p-8 mb-4 rounded-b-lg bg-base drop-shadow place'>
+        <button
+          type='button'
+          className='m-auto col-start-1 col-span-1 hover:text-lavender active:text-opacity-60 ease-in-out duration-200'
+        >
+          <FaSearch size='20px' />
+        </button>{' '}
+        {weatherData && (
+          <span className='text-xl text-center my-auto col-start-2 col-span-4'>
+            {weatherData.name}
+          </span>
+        )}
+        <button
+          type='button'
+          className='m-auto col-start-6 col-span-1 hover:text-peach active:text-opacity-60 ease-in-out duration-200'
+        >
+          <FaRegStar size='20px' />
+        </button>
+        <div className='col-span-6'>
+          <SearchBar />
+        </div>
+      </div>
+    </>
   );
 }
