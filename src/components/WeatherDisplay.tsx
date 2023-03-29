dd978a1035b801ee123b10d16492be58530fb004
Virkkunen <vrkknn@vrkknn.net>
2023-03-28 21:43:53 -0300

    feat: change weather card layout

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -7,33 +7,41 @@ export default function WeatherDisplay() {
   const { weatherData } = useContext(WeatherContext);
   const { main, weather, wind } = weatherData!; // TODO: change to weatherData ?? {} and properly validate
 
-  const iconSrc = `http://openweathermap.org/img/wn/${weather[0].icon}.png`
+  const iconSrc = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
   const windDirection = windDegreeToDirection(wind.deg);
   const windArrow = `rotate-[${wind.deg}deg]`;
 
   return (
     <>
       <div className='m-4 p-8 rounded-lg bg-surface0 flex flex-col place-content-center'>
-
-        <div className='flex flex-col mb-4'>
+        <div className='flex flex-col mb-4 relative'>
           <span className='text-6xl font-mono mb-2'>
             {main.temp.toFixed(0)}°
           </span>
-          <span className='text-sm'>{weather[0].description}, feels like {main.feels_like.toFixed(0)}°</span>
-          <img src={iconSrc} alt={weather[0].description} className='w-24 mx-auto' />
+          <span className='text-sm'>
+            {weather[0].description}, feels like {main.feels_like.toFixed(0)}°
+          </span>
+          <img
+            src={iconSrc}
+            alt={weather[0].description}
+            className='w-24 mx-auto z-0 absolute -top-2 right-0'
+          />
         </div>
 
-        <div className='flex justify-around mb-4'>
-          <span className=''>min: {main.temp_min.toFixed(0)}°</span>
+        <div className='flex mb-4'>
+          <span className='mr-2'>min: {main.temp_min.toFixed(0)}°</span>
           <span className=''>max: {main.temp_max.toFixed(0)}°</span>
         </div>
 
-        <div className='flex justify-between content-center'>
+        <div className='flex flex-col content-center'>
           <span>humidity: {main.humidity}%</span>
-          <span>wind: {wind.speed}km/h {windDirection}</span>
-          <FaArrowUp className={`${windArrow} my-auto`} />
+          <div className='flex flex-row'>
+            <span className='mr-2'>
+              wind: {wind.speed} km/h {windDirection}
+            </span>
+            <FaArrowUp className={`${windArrow} my-auto`} />
+          </div>
         </div>
-
       </div>
     </>
   );
