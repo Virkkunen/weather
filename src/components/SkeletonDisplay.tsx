537d1a53daf4b78b1162f6c305476f3eebf9b281
Virkkunen <vrkknn@vrkknn.net>
2023-03-29 16:10:32 -0300

    chore: skeleton display

---
+++ src/components/SkeletonDisplay.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SkeletonDisplay = () => {
+  return (
+    <div className='m-4 p-8 rounded-lg bg-surface0 flex flex-col place-content-center drop-shadow hover:drop-shadow-lg'>
+      <div className='flex flex-col relative animate-pulse'>
+        <div className='h-12 w-20 bg-overlay0 mb-6 rounded-md' />
+        <div className='h-3 w-48 bg-overlay0 rounded-md' />
+        <div className='h-10 w-10 bg-overlay0 rounded-full z-0 absolute top-2 right-2' />
+        <div className='flex mt-6 mb-6'>
+          <div className='h-3 w-16 bg-overlay0 rounded-md mr-4' />
+          <div className='h-3 w-16 bg-overlay0 rounded-md' />
+        </div>
+        <div className='flex flex-col content-center'>
+          <div className='h-3 w-24 bg-overlay0 rounded-md mb-3' />
+          <div className='h-3 w-36 bg-overlay0 rounded-md mb-0' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonDisplay;
