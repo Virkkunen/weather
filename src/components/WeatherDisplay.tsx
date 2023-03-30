9fe96cdc6109803c2bd10d1056e2f422bb3723ef
Virkkunen <vrkknn@vrkknn.net>
2023-03-30 14:39:49 -0300

    feat: more info and 1.0 release

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 import PreferencesContext from '../context/PreferencesContext';
 import WeatherContext from '../context/WeatherContext';
+import americanTimeToActualTime from '../utils/americanTimeToActualTime';
 import windDegreeToDirection from '../utils/windDegreeToDirection';
 
 export default function WeatherDisplay() {
@@ -36,15 +37,19 @@ export default function WeatherDisplay() {
     }`,
     wind: `${prefs?.units === 'metric' ? current.wind_kph : current.wind_mph}`,
     windUnit: `${prefs?.units === 'metric' ? 'km/h' : 'mp/h'}`,
+    rain: `${
+      prefs?.units === 'metric' ? current.precip_mm : current.precip_in
+    }`,
+    rainUnit: `${prefs?.units === 'metric' ? 'mm' : 'in'}`,
   };
 
   return (
     <div
       className={`m-4 p-8 rounded-lg ${
         current.is_day ? `bg-surface0` : `bg-mantle`
-      } flex flex-col place-content-center drop-shadow hover:drop-shadow-lg md:max-w-md md:mx-auto`}
+      } flex flex-col place-content-center drop-shadow hover:drop-shadow-lg md:max-w-md md:mx-auto gap-2`}
     >
-      <div className='flex flex-col mb-4 relative'>
+      <div className='flex flex-col gap-1 relative'>
         <span className='text-6xl font-mono mb-2'>{units.temp}°</span>
         <span className='text-sm text-subtext1 z-10'>
           {current.condition.text.toLowerCase()}, feels like {units.feelsLike}°
@@ -56,7 +61,7 @@ export default function WeatherDisplay() {
         />
       </div>
 
-      <div className='flex mb-4 gap-2'>
+      <div className='flex gap-2'>
         <span className=''>
           <span className='text-peach font-medium'>max: </span>{' '}
           {units.forecastMax}°
@@ -72,16 +77,41 @@ export default function WeatherDisplay() {
           <span className='text-sapphire font-medium'>humidity: </span>{' '}
           {current.humidity}%
         </span>
+        <div className='flex gap-2'>
+          <span>
+            <span className='text-blue font-medium'>rain: </span> {units.rain}
+            <span className='text-sm'>{units.rainUnit}</span>
+          </span>
+          <span>
+            <span className='text-blue font-medium'>chance: </span>{' '}
+            {forecastday[0].day.daily_chance_of_rain}%{' '}
+          </span>
+        </div>
+      </div>
+
+      <div>
         <div className='flex flex-row gap-2'>
           <span className=''>
             <span className='text-flamingo font-medium'>wind: </span>{' '}
-            {units.wind} {units.windUnit} {windDirection}
+            {units.wind}
+            <span className='text-sm'>{units.windUnit}</span> {windDirection}
           </span>
           <FaArrowUp
             className='my-auto text-flamingo'
             style={{ transform: `rotate(${current.wind_degree}deg)` }}
           />
         </div>
+      </div>
+
+      <div className='flex flex-row gap-2'>
+        <span className=''>
+          <span className='text-yellow font-medium'>sunrise: </span>{' '}
+          {americanTimeToActualTime(forecastday[0].astro.sunrise)}
+        </span>
+        <span className=''>
+          <span className='text-lavender font-medium'>sunset: </span>{' '}
+          {americanTimeToActualTime(forecastday[0].astro.sunset)}
+        </span>
       </div>
     </div>
   );
